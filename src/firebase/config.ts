de988c6e3b9d3611d97437fbddea4effeb6755e7
Nate Johnson <nate.johnson7414@gmail.com>
2025-12-21 13:49:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3124213817-b8545",
+  "appId": "1:274791819112:web:00e2ed85a4a09ab56d18dd",
+  "apiKey": "AIzaSyBFU98js30WfpyNp5ipnRyQjc5I0Sb_UFE",
+  "authDomain": "studio-3124213817-b8545.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "274791819112"
+};
