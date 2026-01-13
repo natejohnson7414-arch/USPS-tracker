1a7b286b51ee130258db26dc9f8db7be7d74c98d
Nate Johnson <nate.johnson7414@gmail.com>
2026-01-13 02:07:16 +0000

    all the text boxes in the work order edit form  details card will only a

--- src/components/work-order-details.tsx
+++ src/components/work-order-details.tsx
@@ -281,8 +281,8 @@ export function WorkOrderDetails({
   return (
     <>
     <form id="work-order-form" onSubmit={onWorkOrderUpdate}>
-      <div className={`grid grid-cols-1 ${!isTechnician ? 'lg:grid-cols-3' : 'lg:grid-cols-2'} gap-8`}>
-        <div className={`${isTechnician ? 'lg:col-span-1' : 'lg:col-span-2'} space-y-8`}>
+      <div className={`grid grid-cols-1 lg:grid-cols-3 gap-8`}>
+        <div className="lg:col-span-2 space-y-8">
           <Card>
             <CardHeader>
               <div className="flex justify-between items-start">
@@ -689,4 +689,5 @@ export function WorkOrderDetails({
     
 
     
+
 
