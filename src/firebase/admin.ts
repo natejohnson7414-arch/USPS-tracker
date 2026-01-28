502c4680f1f8d1811d190ec5adba6bf2084168a6
Nate Johnson <nate.johnson7414@gmail.com>
2026-01-28 21:33:22 +0000

    Try fixing this error: `Console Error: Error: Failed to create auth user

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,10 +1,13 @@
 
 import * as admin from 'firebase-admin';
+import { firebaseConfig } from './config';
 
 // Initialize Firebase Admin SDK, guarding against re-initialization.
 if (!admin.apps.length) {
   try {
-    admin.initializeApp();
+    admin.initializeApp({
+        projectId: firebaseConfig.projectId,
+    });
   } catch (error) {
     console.error('Firebase Admin SDK initialization failed:', error);
   }
