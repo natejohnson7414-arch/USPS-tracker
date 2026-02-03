1d1bba58326eee02eaa2323ce8d9fa87ad5d7acd
Nate Johnson <nate.johnson7414@gmail.com>
2026-02-03 12:58:21 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/work-order-edit-dialog.tsx
+++ src/components/work-order-edit-dialog.tsx
@@ -1,2 +1,8 @@
+'use client';
 
-    
+// This file was causing a server crash because it was empty.
+// It has been replaced with a valid, empty component to resolve the issue.
+// This component is not currently used in the application.
+export default function WorkOrderEditDialog() {
+  return null;
+}
